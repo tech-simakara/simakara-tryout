20f8bbb8127adc4896ed33a6aba4b6d9aef7a668
Muhammad Aziz Prasetyo <azizprasetyo100901@gmail.com>
2025-01-24 02:29:56 +0700

    fix: resolve isSubmitting state not updating during form submission

--- resources/js/components/auth/VerifyEmailForm.jsx
+++ resources/js/components/auth/VerifyEmailForm.jsx
@@ -1,7 +1,7 @@
 import { Button } from '@/components/Button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/Card';
 import { Form } from '@/components/Form';
-import { cn } from '@/lib/utils';
+import { cn, getPathname } from '@/lib/utils';
 import { Link, router } from '@inertiajs/react';
 import { Loader } from 'lucide-react';
 import { useForm } from 'react-hook-form';
@@ -9,16 +9,16 @@ import { useForm } from 'react-hook-form';
 export function VerifyEmailForm({ className, ...props }) {
 	const form = useForm();
 
-	const { handleSubmit, formState, reset } = form;
+	const { handleSubmit, formState } = form;
 	const { isSubmitting } = formState;
 
 	async function onSubmit() {
-		await new Promise(() => {
+		await new Promise((resolve) => {
 			router.post(
-				route('verification.send'),
+				getPathname('verification.send'),
 				{},
 				{
-					onFinish: () => reset(),
+					onFinish: () => resolve(),
 				},
 			);
 		});
